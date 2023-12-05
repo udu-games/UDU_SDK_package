4332b14cbc8e5299a3d0871d53c6dd5a9791ef90
GameDev-Mark <gamedev.markd@gmail.com>
2023-12-05 11:16:54 +0100

    Updated trackpad movement script with latest trackpad updates

--- Samples~/TrackpadMovement/Scripts/BallController.cs
+++ Samples~/TrackpadMovement/Scripts/BallController.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     [SerializeField]private float forceMagnitude; // Adjust this value to control the force applied.
-    private Vector2 trackpadDirection;     // The desired direction to move the ball.
-    private Vector2 trackpadMiddleCoordinates = new Vector2(950f, 600f);     // Coordinate of the trackpad middle, this can different depending on the device
-
     private Rigidbody rb;
 
     private void Start()
@@ -22,7 +19,7 @@ public class BallController : MonoBehaviour
         if (UDUGetters.IsTrackpadPressed())
         {
             // Calculate the new position based on the desired direction.
-            Vector3 movement = new Vector3(TrackpadDirection().x, 0f, -TrackpadDirection().y).normalized * forceMagnitude;
+            Vector3 movement = new Vector3(UDUGetters.GetTrackpadCoordinates().y, 0f, -UDUGetters.GetTrackpadCoordinates().x).normalized * forceMagnitude;
             Vector3 newPosition = rb.position + movement * Time.fixedDeltaTime;
 
             // Use MovePosition to set the new position of the Rigidbody.
@@ -33,12 +30,5 @@ public class BallController : MonoBehaviour
             // If isTrackpadPressed is false, cancel the velocity to stop the object.
             rb.velocity = Vector3.zero;
         }
-    }
-
-    private Vector2 TrackpadDirection()
-    {
-        // Determine direction using the middle coordinates of the trackpad and the current coordinate of the touched area
-        trackpadDirection = new Vector2(UDUGetters.GetTrackpadCoordinates().x, UDUGetters.GetTrackpadCoordinates().y) - trackpadMiddleCoordinates;
-        return trackpadDirection;
     }
 }
